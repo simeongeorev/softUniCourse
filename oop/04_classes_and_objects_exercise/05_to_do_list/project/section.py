59b58d79911cf81ce92599bf47fbf3eee10bc5ef
sgeorev <simeon.georev@scalefocus.com>
2025-10-28 01:35:48 +0200

    oop/04 - 5th done

---
+++ oop/04_classes_and_objects_exercise/05_to_do_list/project/section.py
@@ -0,0 +1,49 @@
+from project.task import Task
+
+class Section:
+
+    def __init__(self, name):
+        self.name = name
+        self.tasks = list()
+
+    def add_task(self, new_task: Task) -> str:
+        if new_task in self.tasks:
+            return f"Task is already in the section {self.name}"
+
+        self.tasks.append(new_task)
+        return f"Task {new_task.details()} is added to the section"
+
+    def complete_task(self, task_name: str) -> str:
+        task = next((t for t in self.tasks if t.name == task_name), None)
+        if not task:
+            return f"Could not find task with the name {task_name}"
+
+        task.completed = True
+        return f"Completed task {task_name}"
+
+    def clean_section(self):
+        removed_count = 0
+        for task in self.tasks:
+            if task.completed:
+                self.tasks.remove(task)
+                removed_count += 1
+
+        return f"Cleared {removed_count} tasks."
+
+    def view_section(self):
+        return (f"Section {self.name}:\n"
+                f"{'\n'.join([x.details() for x in self.tasks])}")
+
+
+task = Task("Make bed", "27/05/2020")
+print(task.change_name("Go to University"))
+print(task.change_due_date("28.05.2020"))
+task.add_comment("Don't forget laptop")
+print(task.edit_comment(0, "Don't forget laptop and notebook"))
+print(task.details())
+section = Section("Daily tasks")
+print(section.add_task(task))
+second_task = Task("Make bed", "27/05/2020")
+section.add_task(second_task)
+print(section.clean_section())
+print(section.view_section())
